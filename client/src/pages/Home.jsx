f4b1a9728990999f92fb9be11507d5dabec3577c
Sushant Shrestha <sushant@kali.kali>
2025-10-17 19:35:18 +0100

    modified Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import About from '../components/About';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 import Footer from '../components/Footer';
-
 
 const Home = () => {
   return (
