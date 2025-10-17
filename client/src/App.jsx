890580ae39c08d09910ffb5f91a502a0b41447e3
Sushant Shrestha <sushant@kali.kali>
2025-10-17 19:05:44 +0100

    added home page to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,8 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
+import Home from './pages/Home';
 
 function App() {
-  const [msg, setMsg] = useState('');
-  useEffect(() => {
-    fetch('http://localhost:5000/')
-      .then(res => res.text())
-      .then(data => setMsg(data));
-  }, []);
-
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-900">
-      <h1 className="text-blue-500 text-3xl font-bold">{msg}</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
