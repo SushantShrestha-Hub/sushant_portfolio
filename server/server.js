d4c7cea7c0d697ab7053f5170ce1444bf15684e7
Sushant Shrestha <sushant@kali.kali>
2025-10-17 18:51:11 +0100

    setup the server side

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+dotenv.config(); 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
